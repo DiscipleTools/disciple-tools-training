e0d30ca35e557de9df521b1bac9b3b8d8369b133
Chris Chasm <chris@chasm.solutions>
2020-04-28 07:06:27 -0600

    Metrics: Code cleanup

--- includes/metrics.php
+++ includes/metrics.php
@@ -29,7 +29,7 @@ class DT_Training_Metrics
         if ( 'metrics' === substr( $url_path, '0', 7 ) ) {
 
             add_filter( 'dt_templates_for_urls', [ $this, 'add_url' ] ); // add custom URL
-            add_filter( 'dt_metrics_menu', [ $this, 'add_menu' ], 50 );
+            add_filter( 'dt_metrics_menu', [ $this, 'add_menu' ], 100 );
 
             if ( 'metrics/trainings' === $url_path ) {
                 add_action( 'wp_enqueue_scripts', [ $this, 'scripts' ], 99 );
@@ -55,7 +55,7 @@ class DT_Training_Metrics
 
     public function add_menu( $content ) {
         $content .= '
-            <li><a href="">' .  esc_html__( 'Training', 'disciple_tools' ) . '</a>
+            <li><a href="">' .  esc_html__( 'Trainings', 'disciple_tools' ) . '</a>
                 <ul class="menu vertical nested" id="training-menu" aria-expanded="true">
                     <li><a href="'. site_url( '/metrics/trainings/' ) .'#cluster_map" onclick="write_training_cluster_map()">'. esc_html__( 'Cluster Map', 'disciple_tools' ) .'</a></li>
                     <li><a href="'. site_url( '/metrics/trainings/' ) .'#points_map" onclick="write_training_points_map()">'. esc_html__( 'Points Map', 'disciple_tools' ) .'</a></li>
@@ -98,14 +98,6 @@ class DT_Training_Metrics
                 ],
             ]
         );
-//        register_rest_route(
-//            $namespace, '/trainings/user_geojson', [
-//                [
-//                    'methods'  => WP_REST_Server::READABLE,
-//                    'callback' => [ $this, 'user_geojson' ],
-//                ],
-//            ]
-//        );
         register_rest_route(
             $namespace, '/trainings/points_geojson', [
                 [
@@ -184,48 +176,6 @@ class DT_Training_Metrics
 
         return $new_data;
     }
-
-//    public function user_geojson( WP_REST_Request $request ) { //@todo get users to populate comparison map
-//        if ( ! $this->has_permission() ){
-//            return new WP_Error( __METHOD__, "Missing Permissions", [ 'status' => 400 ] );
-//        }
-//        $new_data = false;
-//        global $wpdb;
-//
-//        /* pulling 30k from location_grid_meta table */
-//        $results = $wpdb->get_results("
-//            SELECT lg.label as address, p.post_title as name, post_id, lng, lat
-//            FROM $wpdb->dt_location_grid_meta as lg
-//                JOIN $wpdb->posts as p ON p.ID=lg.post_id
-//            WHERE lg.post_type = 'users'
-//            LIMIT 40000
-//            ", ARRAY_A );
-//        $features = [];
-//        foreach ( $results as $result ) {
-//            $features[] = array(
-//                'type' => 'Feature',
-//                'properties' => array(
-//                    "address" => $result['address'],
-//                    "post_id" => $result['post_id'],
-//                    "name" => $result['name']
-//                ),
-//                'geometry' => array(
-//                    'type' => 'Point',
-//                    'coordinates' => array(
-//                        $result['lng'],
-//                        $result['lat'],
-//                        1
-//                    ),
-//                ),
-//            );
-//        }
-//
-//        $new_data = array(
-//            'type' => 'FeatureCollection',
-//            'features' => $features,
-//        );
-//        return $new_data;
-//    }
 
     public function points_geojson( WP_REST_Request $request ) {
         if ( ! $this->has_permission() ){
