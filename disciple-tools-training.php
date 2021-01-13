f6387abdbc826a3fe028524af78bccbe477d282a
Chris Chasm <chris@chasm.solutions>
2021-01-13 13:06:09 -0700

    Add version

--- disciple-tools-training.php
+++ disciple-tools-training.php
@@ -10,7 +10,7 @@
  * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-training
  * Requires at least: 4.7.0
  * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
- * Tested up to: 5.4
+ * Tested up to: 5.6
  *
  * @package Disciple_Tools
  * @link    https://github.com/DiscipleTools
