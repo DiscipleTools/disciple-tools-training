25277d115a40678468437d5c5f3fcc31ff505b2f
Mike Allbutt <yo@ur.id.au>
2022-03-28 13:17:51 -0600

    Remove reference to Prayer Subscription

--- post-type/module-base.php
+++ post-type/module-base.php
@@ -4,8 +4,8 @@ if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
 class DT_Training_Base extends DT_Module_Base {
     public $post_type = "trainings";
     public $module = "trainings_base";
-    public $single_name = 'Prayer Subscription';
-    public $plural_name = 'Subscriptions';
+    public $single_name = 'Training';
+    public $plural_name = 'Trainings';
     public static function post_type(){
         return 'prayers';
     }
